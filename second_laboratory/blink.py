1b7b8377ff9c9f70ff8611ad2580cbbd86ceb9a9
HEPL-Mineur <martinmineur3@gmail.com>
2025-10-09 20:25:09 +0200

    addition of comments

--- second_laboratory/blink.py
+++ second_laboratory/blink.py
@@ -1,14 +1,19 @@
 import machine  
 import utime 
 
+# initialization of the different pins
 Rotary_sensor = machine.ADC(0)
 buzzer = machine.PWM(machine.Pin(16))
-button = machine.Pin(20, machine.Pin.IN)
-score = {"DO":[1046, 250],"MI" :[1318, 250], "FA" :[1397, 250],
-        "RE":[1175,250], "SOL" : [1568, 500], "LA" :[1760, 125],
-        "SI":[1967, 500] }
+button = machine.Pin(20, machine.Pin.IN) 
 
-avengers_theme = [
+# frequencies and time of play of every musical note
+score = {"DO":[1046, 250],"MI" :[1318, 250], "FA" :[1397, 250], 
+        "RE":[1175,250], "SOL" : [1568, 500], "LA" :[1760, 125],
+        "SI":[1967, 500] } 
+
+#<----------------------------------->
+# the musical note of different music
+avengers_theme = [ 
     "FA", "FA", "LA", "FA",
     "RE", "DO", "RE",
     "FA", "FA", "LA", "FA",
@@ -40,36 +45,55 @@ super_mario_theme = [
     "SOL", "LA", "FA", "SOL"
 ]
 
-
 star_wars_theme_simplifie = [
     "SOL", "SOL", "SOL", "MI",
     "SI", "SOL", "MI", "SI", "SOL",
     "RE", "RE", "RE", "MI", "SI", "FA", "MI", "SI", "SOL"
 ]
+#<////////////////////////////////////>
 
+
+#<----------------------------------->
+# A list of the different musical notes of music and the name of each music
 nom_musique = [star_wars_theme_simplifie,super_mario_theme,harry_potter_theme,jurassic_park_theme, pirates_theme,avengers_theme]
 nom_musique_prime = ["star_wars_theme_simplifie","super_mario_theme","harry_potter_theme","jurassic_park_theme", "pirates_theme","avengers_theme"]
+#<////////////////////////////////////>
 
+#<----------------------------------->
+# function which is used to play the musicla notes
+# score is the dictionnary which has the information of musical notes
+# notes is the musical which is currently plays
+# last hs information over time when the last musical note was played
+# i is the  index incrementation of the muscial notes
+# new is the time 
+# if the difference of new - last is over the time of the notes we can change the muscal note
+# resistance is the value of the Rotary Angle Sensor to determine the intensity of the song
 def music_notes(score, notes, last, i) :
     new = utime.ticks_ms() 
-    #print(new-last)
     if score[notes][1]<(new - last):
-        print("rentre")
         resistance = Rotary_sensor.read_u16() 
         intensity = int(resistance*0.30518)
         buzzer.freq(score[notes][0])
         buzzer.duty_u16(intensity)
         return utime.ticks_ms(), i+1
     return last, i
-        
+#<////////////////////////////////////>
 
+
+#<----------------------------------->
 ref_musique = 0
 ancien_index =0
 i = 0
 changement = False
 last = utime.ticks_ms()
 last_button= utime.ticks_ms()
+#<////////////////////////////////////>
+
+#<----------------------------------->
+# infinity loop to play the music
 while True:
+
+    # look to check the value of the button because the incrementation can be uncontrollable
     index = button.value()
     new_button = utime.ticks_ms()
     if  250 < (new_button-last_button) and index==1:
@@ -81,18 +105,16 @@ while True:
         last_button= utime.ticks_ms()
     else :
         changement = False
-
     ancien_index = index
-    print(nom_musique_prime[ref_musique])
-    while i != (len(nom_musique[ref_musique])) and button.value()!=1 and changement ==False:
 
+    # check the name of the music
+    print(nom_musique_prime[ref_musique])
+
+    # loop for the playins of an entire music
+    while i != (len(nom_musique[ref_musique])) and button.value()!=1 and changement ==False:
         musique = nom_musique[ref_musique]
         Note = musique[i]
         last, i =music_notes(score, Note, last, i)
         
-    print("sort")
     i = 0
-    
- 
-   
-    
+#<////////////////////////////////////>
