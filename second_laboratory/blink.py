3f89b7ea97091c28dd86ad9391957d6179486ab7
HEPL-Mineur <martinmineur3@gmail.com>
2025-10-09 14:47:37 +0200

    utilisation of duty
    Ajustement du volume avec la fonction duty_u16()

--- second_laboratory/blink.py
+++ second_laboratory/blink.py
@@ -1,14 +1,33 @@
-from machine import Pin
-from utime import sleep
+import machine  
+import utime 
 
-pin = Pin("LED", Pin.OUT)
+Rotary_sensor = machine.ADC(0)
+buzzer = machine.PWM(machine.Pin(16))
+score = {"DO":[1046, 0.25],"MI" :[1318, 0.25], "FA" :[1397, 0.25],
+        "RE":[1175,0.25], "SOL" : [1568, 0.5], "LA" :[1760, 0.125],
+        "SI":[1967, 0.5] }
+star_wars_theme_simplifie = [
+    "SOL", "SOL", "SOL", "MI",
+    "SI", "SOL", "MI", "SI", "SOL",
+    "RE", "RE", "RE", "MI", "SI", "FA", "MI", "SI", "SOL"
+]
 
-print("LED starts flashing...")
+def music_notes(score, notes) : 
+    resistance = Rotary_sensor.read_u16() 
+    intensity = int(resistance*0.30518)
+    print(resistance)
+    print(intensity)
+    buzzer.freq(score[notes][0])
+    buzzer.duty_u16(intensity)
+    utime.sleep(score[notes][1])
+
 while True:
-    try:
-        pin.toggle()
-        sleep(1) # sleep 1sec
-    except KeyboardInterrupt:
-        break
-pin.off()
-print("Finished.")
+    #print(Rotary_sensor.read_u16())
+    # divide in 16 states of reading by 4096 8192 12288 16384 20480 24576 28672 32768 38864 40960 45056 49152 53248 57344 61440 last
+    for i in range (len(star_wars_theme_simplifie)):
+        Note = star_wars_theme_simplifie[i]
+        music_notes(score, Note)
+    
+ 
+   
+    
